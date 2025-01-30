bf12d82fb0e3963b8f07bde5f71b5ae140861bac
Jerry Lyu <45759373+astro2049@users.noreply.github.com>
2025-01-30 17:26:31 +0000

    skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css'
+import Navbar from "./components/navbar/Navbar.tsx";
+import Main from "./components/main/Main.tsx";
+import Footer from "./components/footer/Footer.tsx";
 
 function App() {
-  return (
-    <>
-      Resident Evil
-    </>
-  )
+    return (
+        <>
+            <Navbar></Navbar>
+            <Main></Main>
+            <Footer></Footer>
+        </>
+    )
 }
 
 export default App
